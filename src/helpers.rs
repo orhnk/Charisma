a923f040e51215f7fb092625fe946b3ee3ae29cc
utfeight <utfeightt@gmail.com>
2023-07-21 01:52:25 +0300

    Fixed: Clippy Warnings

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,21 +1,16 @@
 use crate::utils::HEADERS;
 use serde::Serialize;
 
-pub async fn send_req<T>(
-    url: &str,
-    json: &T,
-) -> Result<reqwest::Response, reqwest::Error>
+pub async fn send_req<T>(url: &str, json: &T) -> Result<reqwest::Response, reqwest::Error>
 where
     T: Serialize + ?Sized,
 {
     let client = reqwest::Client::new();
 
-    let res = client
+    client
         .post(url)
         .json(json)
         .headers(HEADERS.clone()) // FIXME
         .send()
         .await;
-
-    res
 }
